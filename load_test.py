5a0727cedcd93ef6777d4781c531dcc2cfcd73fe
elenchi <mailjpk@gmail.com>
2019-06-21 12:49:06 +1000

    Maybe actually fixed

--- load_test.py
+++ load_test.py
@@ -1,8 +1,8 @@
 import FileUtils
-import pandas as pd
-from timeit import timeit
+import pandas
+import timeit as tm
 
 filename = FileUtils.get_mbs_files()[0]
 col = 'PIN'
-x = timeit(pd.read_parquet(filename, columns=[col]))
+x = tm.timeit(lambda: pandas.read_parquet(filename, columns=[col]))
 print(x)
