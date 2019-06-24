8d4037d90ebd949a9fa4b9e750692d498a1ab7e3
Elenchus <mailjpk@gmail.com>
2019-06-24 11:16:30 +1000

    minor corrections

--- determine_numserv_usage.py
+++ determine_numserv_usage.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     years = []
     for filename in filenames:
         year = re.search("_(\d\d\d\d)\.", filename)[1]
-        data = pd.read_parquet(filename, columns = ['NUMSERV', 'SPR_RSP'])
+        df = pd.read_parquet(filename, columns = ['NUMSERV', 'SPR_RSP'])
         rows_of_interest = df.loc[df['SPR_RSP'] == rsp.convert_str("Not defined")]
         unique_values = rows_of_interest['NUMSERV'].unique()
         frequencies = rows_of_interest.values.tolist()
 
         frequencies_by_year.append(frequencies)
-        labels_by_year.appendO(unique_values)
+        labels_by_year.append(unique_values)
         years.append(year)
 
     FileUtils.categorical_plot_group(logger, labels_by_year, frequencies_by_year, years, "NUMSERV frequencies for Not Defined Provider Specialties", "numserv_not_defined")
