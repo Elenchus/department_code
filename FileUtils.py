bd6e62fbbd724290d4e43940c1ddd5afccb9cd23
Elenchus <mailjpk@gmail.com>
2019-06-28 17:22:04 +1000

    Added pbs item map

--- FileUtils.py
+++ FileUtils.py
@@ -132,27 +132,37 @@ def tsne_plot(logger, model, perplex):
     logger.info(f"Saving TSNE figure to {path}")
     fig.savefig(path)
 
-class spr_rsp_converter:
+class code_converter:
     def __init__(self):
-        filename = 'SPR_RSP.csv'
-        if not os.path.isfile(filename):
+        rsp_filename = 'SPR_RSP.csv'
+        pbs_item_filename = 'pbs-item-drug-map.csv'
+
+        if not os.path.isfile(rsp_filename):
             raise OSError("Cannot find SPR_RSP.csv - please put it in the same folder as FileUtils")
 
-        self.table = pd.read_csv(filename)
-        self.valid_num_values = self.table['SPR_RSP'].unique()
-        self.valid_str_values = self.table['Label'].unique()
+        if not os.path.isfile(pbs_item_filename):
+            raise OSError("Cannot find pbs-item-drug-map.csv - please put it in the same folder as FileUtils")
 
-    def convert_num(self, rsp):
-        if int(rsp) not in self.valid_num_values:
+        self.rsp_table = pd.read_csv(rsp_filename)
+        self.pbs_item_table = pd.read_csv(pbs_item_filename, dtype=str, encoding = "latin")
+        self.valid_rsp_num_values = self.rsp_table['SPR_RSP'].unique()
+        self.valid_rsp_str_values = self.rsp_table['Label'].unique()
+
+    def convert_pbs_code(self, code):
+
+        return self.pbs_item_table.loc[self.pbs_item_table['ITEM_CODE'] == code]
+
+    def convert_rsp_num(self, rsp):
+        if int(rsp) not in self.valid_rsp_num_values:
             raise ValueError(f"{rsp} is not a valid SPR_RSP")
 
-        return self.table.loc[self.table['SPR_RSP'] == int(rsp)]['Label'].values.tolist()[0]
+        return self.rsp_table.loc[self.rsp_table['SPR_RSP'] == int(rsp)]['Label'].values.tolist()[0]
 
-    def convert_str(self, rsp):
-        if str(rsp) not in self.valid_str_values:
+    def convert_rsp_str(self, rsp):
+        if str(rsp) not in self.valid_rsp_str_values:
             raise ValueError(f"{rsp} is not a valid name")
 
-        return self.table.loc[self.table['Label'] == str(rsp)]['SPR_RSP'].values.tolist()[0]
+        return self.rsp_table.loc[self.rsp_table['Label'] == str(rsp)]['SPR_RSP'].values.tolist()[0]
     
 
 class logger:
