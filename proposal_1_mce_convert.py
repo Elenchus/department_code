132905b3ea26f3802865f55b5595946043bf418b
Elenchus <mailjpk@gmail.com>
2019-07-24 15:32:53 +1000

    Bug fixes

--- proposal_1_mce_convert.py
+++ proposal_1_mce_convert.py
@@ -6,8 +6,8 @@ from datetime import datetime as dt
 logger = FileUtils.logger(__name__, "proposal_1_mce_conversion", '/mnt/c/data')
 output_file = logger.output_path / 'hip_replacement_mce.txt'
 data = pd.read_csv('hip_subset.csv').values.tolist()
-pid, item, dos = map(data)
-dos = [dt.strptime(x, "%d%b%Y").timestamp() for x in dos]
+pid, item, dos = zip(*data)
+dos = [dt.strptime(x, "%d/%m/%Y").timestamp() for x in dos]
 
 current_pid = pid[0] 
 current_date = dos[0]
