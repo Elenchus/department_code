e9fcaaf1fe9b9ead979f48d9b50ea45664912c2a
Elenchus <mailjpk@gmail.com>
2019-07-29 10:43:44 +1000

    Updated provider specialty clustering for FileUtils updates

--- embed_provider_specialty_item.py
+++ embed_provider_specialty_item.py
@@ -57,24 +57,24 @@ if __name__ == "__main__":
             no_unique_rsp = no_unique_rsp - 1
 
     logger.log(f"Clustering with {no_unique_rsp} clusters")
-    # cluster_no = [32, 64, 96, 128, no_unique_rsp, 160, 192, 224, 256]
-    # best_cluster = FileUtils.get_best_cluster_size(logger, cluster_no)
+    cluster_no = [32, 64, 96, 128, no_unique_rsp, 160, 192, 224, 256]
+    k, score = FileUtils.get_best_cluster_size(logger, X, cluster_no)
 
     kmeans = cluster.KMeans(n_clusters=no_unique_rsp)
     kmeans.fit(X)
     labels = kmeans.labels_
     # centroids = kmeans.cluster_centers_
 
-    logger.log("Plotting t-SNE and k-means")
-    tsne_plot(logger, model, no_unique_rsp)
+    # logger.log("Plotting t-SNE and k-means")
+    # FileUtils.tsne_plot(logger, model, no_unique_rsp, "t-")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    scatter = ax.scatter(X[:, 0], X[:, 1], c=labels)
-    legend = ax.legend(*scatter.legend_elements(), loc="upper left", title="Cluster no.")
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # scatter = ax.scatter(X[:, 0], X[:, 1], c=labels)
+    # legend = ax.legend(*scatter.legend_elements(), loc="upper left", title="Cluster no.")
 
-    fig.savefig(logger.output_path + "items_and_specialties_k-means_" + datetime.now().strftime("%Y%m%dT%H%M%S"))
-
+    # fig.savefig(logger.output_path + "items_and_specialties_k-means_" + datetime.now().strftime("%Y%m%dT%H%M%S"))
+    FileUtils.create_scatter_plot(logger, X, labels, "Item/provider specialty clusters", "items_and_specialties_k_means")
     logger.log("Re-loading parquet file")
     data = pd.read_parquet(filename, columns=['PIN', 'ITEM', 'SPR_RSP']).astype(str)
 
