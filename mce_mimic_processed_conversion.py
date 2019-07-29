1812e85c6c65ed2b12af0f0a53751a58f035cdc2
Elenchus <mailjpk@gmail.com>
2019-07-29 14:48:00 +1000

    Bug fixes, added files for Sebastiano. Sort method not working

--- mce_mimic_processed_conversion.py
+++ mce_mimic_processed_conversion.py
@@ -2,6 +2,7 @@ from math import ceil
 import numpy as np
 
 path = '/home/elm/data/preprocessed_mimic_data/'
+# files = [('charts_prescriptions.csv', ["ICUSTAY_ID", "VALUECAT", "HOURS_TO_OUT"])]
 files = [('diag_proc.csv', ["ICUSTAY_ID", "ICD9_CODE", "DAYS_TO_OUT"]), ('charts_prescriptions.csv', ["ICUSTAY_ID", "VALUECAT", "HOURS_TO_OUT"])]
 
 def convert_to_seconds(timestamp, header):
@@ -17,15 +18,33 @@ for (name, header) in files:
     with open(path + name, 'r') as input_file, open(output_file_path, 'w+') as output:
         current_id = None
         current_time = None
+        line = None
+        x = ",".join(header) + '\n'
         for each_line in input_file:
-            row = [x.strip() for x in each_line.split(',')]
+            if each_line == x:
+                continue
+
+            row = [x.strip('\n') for x in each_line.split(',')]
+            if row[1] == '':
+                continue
+
             if row[0] != current_id:
                 current_id = row[0]
-                current_time = convert_to_seconds(row[2], header[2])
-                output.write(f"{line}]]]\r\n")
+                if row[1] == '':
+                    current_time = 0
+                else:
+                    current_time = convert_to_seconds(row[2], header[2])
+                
+                if line is not None:
+                    output.write(f"{line}]]]\r\n")
+
                 line = f"{current_id}, [[{current_time}, [{row[1]}"
             else:
-                new_time = convert_to_seconds(row[2], header[2])
+                if row[2] == '':
+                    new_time = current_time
+                else:
+                    new_time = convert_to_seconds(row[2], header[2])
+
                 if new_time != current_time:
                     current_time = new_time
                     line = f"{line}], {current_time}, [{row[1]}"
