2734083ddcde042c6fc2da1f6e87cea6e689d504
Elenchus <mailjpk@gmail.com>
2019-08-21 17:03:07 +1000

    Minor refactor

--- test_runner.py
+++ test_runner.py
@@ -4,17 +4,7 @@ from functools import partial
 from phd_utils import file_utils
 from phd_utils.logger import Logger
 
-if __name__ == "__main__":
-    # variables
-    years = ['2003']
-    data_file = None
-    test_data = 'mbs'
-    proposal = 2
-    test_file_name = 'proposal_2_cluster_providers'
-    params = None
-    notes = "Testing re-factor"
-
-    # start test
+def run_test(years, data_file, test_data, proposal, test_file_name, params, notes):
     test_name = f'proposal_{proposal}_{test_file_name}_{test_data}_{years[0] if len(years) == 1 else f"{years[0]}-{years[-1]}"}'
     logger = Logger(__name__, test_name, '/mnt/c/data')
     test_file = __import__(f"proposal_{proposal}.{test_file_name}", fromlist=['TestCase'])
@@ -31,3 +21,17 @@ if __name__ == "__main__":
 
     test_data = test_case.get_test_data(data)
     test_case.run_test(test_data, params)
+
+    return (data, test_data, test_case)
+
+if __name__ == "__main__":
+    # variables
+    years = ['2003']
+    data_file = None
+    test_data = 'mbs'
+    proposal = 2
+    test_file_name = 'proposal_2_cluster_providers'
+    params = None
+    notes = "Testing re-factor"
+
+    run_test(years, data_file, test_data, proposal, test_file_name, params, notes)
