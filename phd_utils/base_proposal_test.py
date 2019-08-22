57d4ab38d257d4b273f9baeb65b689e0ca5eb95a
Elenchus <mailjpk@gmail.com>
2019-08-22 13:08:19 +1000

    Test drafted

--- phd_utils/base_proposal_test.py
+++ phd_utils/base_proposal_test.py
@@ -21,19 +21,35 @@ class ProposalTest(ABC):
     def REQUIRED_PARAMS(self):
         raise NotImplementedError
 
+    @property
+    @classmethod
+    @abstractmethod
+    def processed_data(self):
+        raise NotImplementedError
+
+    @property
+    @classmethod
+    @abstractmethod
+    def test_data(self):
+        raise NotImplementedError
+    
     def __init__(self, logger):
         self.logger = logger
         self.graphs = GraphUtils(logger)
         self.models = ModelUtils(logger)
     
     @abstractmethod
-    def process_dataframe(self, logger, data, params=None):
-        pass
+    def process_dataframe(self, data):
+        self.logger.log("Processing dataframe")
 
     @abstractmethod
-    def get_test_data(self, logger, data, params=None):
-        pass
+    def get_test_data(self):
+        self.logger.log("Getting test data")
+        if self.processed_data is None:
+            raise KeyError("No data specified")
 
     @abstractmethod
-    def run_test(self, logger, data, params=None):
-        pass
+    def run_test(self):
+        self.logger.log("Running test")
+        if self.test_data is None:
+            raise KeyError("No test data specified")
--- proposal_2/number_of_providers_using_each_specialty.py
+++ proposal_2/number_of_providers_using_each_specialty.py
@@ -5,12 +5,27 @@ from phd_utils.base_proposal_test import ProposalTest
 from phd_utils.code_converter import CodeConverter
 
 class TestCase(ProposalTest):
-    INITIAL_COLS = ["SPR", "SPR_RSP"]
+    INITIAL_COLS = ["SPR_RSP", "SPR"]
     FINAL_COLS = INITIAL_COLS
     REQUIRED_PARAMS: dict = {}
-    unprocessed_data: pd.DataFrame
     processed_data: pd.DataFrame 
     test_data = None
 
-    def process_dataframe(self):
-        data = self.unprocessed_data
+    def process_dataframe(self, data):
+        super().process_dataframe(data) 
+
+    def get_test_data(self):
+        super().get_test_data()
+        self.test_data = self.processed_data
+
+    def run_test(self):
+        super().run_test()
+        data = self.test_data.groupby("SPR_RSP")
+        providers_per_rsp = []
+        for rsp, group in data:
+            providers_per_rsp.append((rsp, len(group['SPR'].unique)))
+
+        with open(self.logger.output_path / "providers_per_rsp.txt") as f:
+            for (rsp, n) in providers_per_rsp:
+                f.write(f"{rsp}: {n}\r\n")
+
