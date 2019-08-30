9721c15fc2fdb1adf225b017ec6b327ab6166225
Elenchus <mailjpk@gmail.com>
2019-08-30 14:40:50 +1000

    Drafted test

--- proposal_2/predict_specialty_from_item_set.py
+++ proposal_2/predict_specialty_from_item_set.py
@@ -3,6 +3,8 @@ import itertools
 import pandas as pd
 from phd_utils.base_proposal_test import ProposalTest
 from phd_utils.code_converter import CodeConverter
+from sklearn.model_selection import KFold
+from sklearn.naive_bayes import MultinomialNB
 
 class TestCase(ProposalTest):
     '''test class'''
@@ -24,17 +26,32 @@ class TestCase(ProposalTest):
         super().get_test_data()
         cdv = CodeConverter()
         groups = itertools.groupby(sorted(self.processed_data), key=lambda x: x[0])
-        test_data = {}
+        provider_list, item_list, rsp_list = [], [], []
         for spr, group in groups:
             (_, items, rsps) = tuple(set(x) for x in zip(*list(group)))
-            test_data[spr] = {}
-            test_data[spr]["Items"] = items
+            provider_list.append(spr)
+            item_list.append(items)
             if len(rsps) > 1:
                 rsps = "Multiple RSPs"
             else:
                 rsps = cdv.convert_rsp_str(list(rsps)[0])
 
-        self.test_data = test_data
+            rsp_list.append(rsps)
+
+        self.test_data = [provider_list, item_list, rsp_list]
 
     def run_test(self):
         super().run_test()
+        clf = MultinomialNB()
+        items = self.test_data[1]
+        labels = self.test_data[2]
+        kf = KFold(n_splits=5)
+        for train_index, test_index in kf.split(labels):
+            X_train, X_test = items[train_index], items[test_index]
+            y_train, y_test = labels[train_index], labels[test_index]
+            clf.fit(X_train, y_train)
+            acc = clf.score(X_test, y_test)
+            self.log(f"Accuracy: {acc}")
+
+        acc = clf.score(items, labels)
+        self.log(f"Final accuracy: {acc}")
