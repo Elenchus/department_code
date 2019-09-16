9f52f9cf1da15eff6e1309941fa736bd72865dcf
Elenchus <mailjpk@gmail.com>
2019-09-16 12:52:51 +1000

    Fixed label order problem

--- proposal_2/simple_provider-patient-item_clusters.py
+++ proposal_2/simple_provider-patient-item_clusters.py
@@ -11,20 +11,21 @@ class TestCase(ProposalTest):
     test_data = None
 
     def get_item_labels(self, vocab):
-        labels = []
+        labels = {}
         frequencies = []
         data = sorted(self.processed_data[["ITEM", "SPR_RSP"]].values.tolist())
         groups = itertools.groupby(data, key=lambda x: x[0])
         for item, group in groups:
-            if str(item) in vocab:
+            item = str(item)
+            if item in vocab:
                 group = [x[1] for x in group]
                 most_common = max(set(group), key=group.count)
                 # labels.append(self.code_converter.convert_rsp_num(most_common))
                 uses = list(set(group))
                 if len(uses) == 1:
-                    labels.append(uses[0])
+                    labels[item] = self.code_converter.convert_rsp_num(uses[0])
                 else:
-                    labels.append("Mixed")
+                    labels[item] ="Mixed"
 
                 ratio = round(group.count((most_common)) / len(group), 1)
                 frequencies.append(ratio)
@@ -50,50 +51,67 @@ class TestCase(ProposalTest):
     def get_test_data(self):
         super().get_test_data()
         self.log("Sorting data")
-        patient_data = sorted(self.processed_data[["PIN", "SPR", "ITEM"]].values.tolist())
+        # patient_data = sorted(self.processed_data[["PIN", "SPR", "ITEM"]].values.tolist())
+        patient_data = sorted(self.processed_data.values.tolist())
         self.log("Grouping data")
         patient_groups = itertools.groupby(patient_data, key=lambda x: x[0])
         sentences = []
+        rsps = []
         self.log("Creating sentence/label lists")
-        for _, patient_group in patient_groups:
+        for pin, patient_group in patient_groups:
             patient_group = list(patient_group)
             provider_data = sorted([x[1:] for x in patient_group])
             provider_groups = itertools.groupby(provider_data, key=lambda x: x[0])
-            for _, provider_group in provider_groups:
+            for spr, provider_group in provider_groups:
                 provider_group = list(provider_group)
                 sentence = list(set(str(x[1]) for x in provider_group))
+                rsp = list(set(str(x[2]) for x in provider_group))
                 if len(sentence) > 1:
                     sentences.append(sentence)
+                    rsps.append(rsp)
 
-        self.test_data = sentences
+        self.test_data = (sentences, rsps)
                     
     def run_test(self):
         super().run_test()
-        sentences = self.test_data
+        (sentences, rsps) = self.test_data
         unique_item_sentences = [list(set(x)) for x in sentences]
         max_sentence_length = max([len(x) for x in unique_item_sentences])
         self.log("Creating model")
         model = Word2Vec(unique_item_sentences, size = self.required_params['size'], window=max_sentence_length, min_count=1)
-        (vocab_labels, frequencies) = self.get_item_labels(model.wv.vocab)
-        with open(self.logger.output_path / 'labels', 'w+') as f:
-            assert len(model.wv.vocab) == len(vocab_labels)
-            words = list(model.wv.vocab.keys())
+        word_list = list(model.wv.vocab.keys())
+        (vocab_labels_dict, frequencies) = self.get_item_labels(word_list)
+        vocab_labels = []
+        for word in word_list:
+            vocab_labels.append(vocab_labels_dict[word])
+
+        with open(self.logger.output_path / 'labels.txt', 'w+') as f:
+            assert len(word_list) == len(vocab_labels)
             for i in range(len(vocab_labels)):
-                f.write(f'"{words[i]}":"{vocab_labels[i]}"\r\n')
+                f.write(f'"{word_list[i]}":"{vocab_labels[i]}"\r\n')
 
         self.graphs.basic_histogram(frequencies, 'hist_gram')
-        # labels, legend_names = pd.factorize(vocab_labels)
-        # vectors = []
-        # for word in model.wv.vocab:
-        #     vectors.append(model.wv.get_vector(word))
+        labels, legend_names = pd.factorize(vocab_labels)
+        vectors = []
+        for word in word_list:
+            vectors.append(model.wv.get_vector(word))
 
-        # self.log("Transforming")
-        # output = self.models.pca_2d(vectors)
-        # no_unique_points = len(list(set(tuple(p) for p in output)))
-        # self.log(f"Set of 2d transformed provider vectors contains {no_unique_points} unique values from {output.shape[0]} samples")
-        # # self.models.k_means_cluster(output, 256, f"provider {name} k-means", f"provider_{name}_kmeans", labels)
-        # self.graphs.create_scatter_plot(output, labels, f"item scatter plot", f"item_scatter", legend_names)
+        self.log("Transforming")
+        output = self.models.pca_2d(vectors)
+        no_unique_points = len(list(set(tuple(p) for p in output)))
+        self.log(f"Set of 2d transformed provider vectors contains {no_unique_points} unique values from {output.shape[0]} samples")
+        # self.models.k_means_cluster(output, 256, f"provider {name} k-means", f"provider_{name}_kmeans", labels)
+        self.graphs.create_scatter_plot(output, labels, f"item scatter plot", f"item_scatter", legend_names)
 
-        # with open(self.logger.output_path / 'sentences.txt', 'w+') as f:
-        #     for sentence in self.test_data:
-        #         f.write(f"{sentence}\r\n")
+        with open(self.logger.output_path / 'sentence_rsps.txt', 'w+') as f:
+            for sentence in sentences:
+                rsp_list = []
+                for word in sentence:
+                    label = vocab_labels[word_list.index(word)]
+                    rsp_list.append(label)
+
+                f.write(f"{rsp_list}\r\n")
+
+        with open(self.logger.output_path / 'rsps.txt', 'w+') as f:
+            for rsp in rsps:
+                f.write(f"{rsp}\r\n")
