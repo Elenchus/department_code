7fd408bd14d12385a35d3c27d74e2ccae6335197
Elenchus <mailjpk@gmail.com>
2019-09-17 12:01:05 +1000

    Added unit test for cartesian to polar

---
+++ tests/test_model_utils.py
@@ -0,0 +1,35 @@
+import math
+import unittest
+import phd_utils.model_utils as model_utils
+from pathlib import Path
+
+class MockLogger():
+    output_path = Path('')
+    test_name = 'Mock'
+
+    def log(self, line, line_end):
+        pass
+
+class ModelUtilsTest(unittest.TestCase):
+    def __init__(self):
+        self.model = model_utils.ModelUtils(MockLogger())
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_cartesian_polar(self):
+        data = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+        polar = self.model.cartesian_to_polar(data)
+        for result in polar:
+            assert result[0] == math.sqrt(2)
+
+        assert polar[0][1] == 45
+        assert polar[1][1] == 135
+        assert polar[2][1] == 225
+        assert polar[3][1] == 315
+
+if __name__ == "__main__":
+    unittest.main()
