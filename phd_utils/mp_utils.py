f20dbbbebe9d6c96feba52708ca35cf8466679e1
Elenchus <mailjpk@gmail.com>
2019-11-20 15:13:04 +1100

    Tried to add multiprocessing - getting err 22

--- phd_utils/mp_utils.py
+++ phd_utils/mp_utils.py
@@ -1,8 +1,8 @@
 from tqdm import tqdm
-from multiprocessing import cpu_count, Pool
+from multiprocessing import Pool
 
 def multi_grouper(func, grouper, callback = None):
-    pool = Pool(processes=cpu_count())
+    pool = Pool()
     for result in tqdm(pool.imap(func, grouper)):
         if callback is not None:
             callback(result)
--- proposal_1/data_extraction.py
+++ proposal_1/data_extraction.py
@@ -2,9 +2,9 @@ import itertools
 import pandas as pd
 from datetime import datetime as dt
 from datetime import timedelta
+from functools import partial
 from phd_utils.base_proposal_test import ProposalTest
 from phd_utils.mp_utils import multi_grouper
-from tqdm import tqdm
 
 class TestCase(ProposalTest):
     hip_replacement_codes_of_interest = ['49309','49312', '49315',' 49318','49319', '49321', '49324', '49327', '49330', '49333', '49336', '49339', '49342', '49345','49346', '49360', '49363', '49366']
@@ -16,12 +16,15 @@ class TestCase(ProposalTest):
     processed_data: pd.DataFrame = None
     test_data = None
 
-    def append_to_file(self, file, data):
-        with open(file, 'a') as f:
-            data.to_csv(f, header=f.tell()==0)
+    @staticmethod
+    def append_to_file(file, data):
+        if data is not None:
+            with open(file, 'a') as f:
+                data.to_csv(f, header=f.tell()==0)
 
-    def extract_relevant_claims(self, header, code_list, group):
-        claims = pd.DataFrame(list(group), columns=header)
+    @staticmethod
+    def extract_relevant_claims(header, code_list, group):
+        claims = pd.DataFrame(list(group[1]), columns=header)
         dates_of_interest = claims.loc[claims['ITEM'].isin(code_list), 'DOS'].values.tolist()
         if len(dates_of_interest) == 0:
             return None
@@ -50,7 +53,6 @@ class TestCase(ProposalTest):
         output_file = self.logger.output_path / name
 
         self.logger.log("Extracting claims")
-        for _, group in tqdm(self.test_data):
-            relevant_claims = self.extract_relevant_claims(self.processed_data.columns, self.required_params['codes_of_interest'], group)
-            if relevant_claims is not None:
-                self.append_to_file(output_file, relevant_claims)
+        claims_func = partial(self.extract_relevant_claims, [self.processed_data.columns, self.required_params["codes_of_interest"]])
+        file_writer = partial(self.append_to_file, [output_file])
+        multi_grouper(claims_func, self.test_data, file_writer)
