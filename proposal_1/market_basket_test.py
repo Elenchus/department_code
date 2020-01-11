8928c2d2fd6413278d0931feb32837bb43c89716
Elenchus <mailjpk@gmail.com>
2020-01-11 12:30:35 +1100

    Added market basket test

---
+++ proposal_1/market_basket_test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from apyori import apriori
+
+input_file = 'synthetic_sentences_prop_1.csv'
+data = pd.read_csv(input_file)
+patients = data.groupby("PIN")
+
+documents = []
+for name, group in patients:
+    items = group["ITEM"].values.tolist()
+    items = [str(item) for item in items]
+    documents.append(items)
+
+rules = apriori(documents, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+print(list(rules))
