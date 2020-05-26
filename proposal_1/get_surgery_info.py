f101e958943326a41e9bd1e5c1fa434fb8854c21
Elenchus <mailjpk@gmail.com>
2020-05-26 12:12:41 +1000

    Extra options for surgery info

--- proposal_1/get_surgery_info.py
+++ proposal_1/get_surgery_info.py
@@ -6,10 +6,10 @@ from tqdm import tqdm
 class TestCase(ProposalTest):
     @dataclass
     class RequiredParams:
-        anaesthetic_code:int = 21638
-        surgery_code_start:int = 48900 
-        surgery_code_end:int = 49000
-        surgery_description:str = "elbow"
+        anaesthetic_code:int = None 
+        surgery_code_start:int = 48600 
+        surgery_code_end:int = 48900
+        surgery_description:str = "spine"
         alternate_code_check_start:int = None
         alternate_code_check_end:int = None
 
@@ -41,59 +41,59 @@ class TestCase(ProposalTest):
         patient_groups = patient_claims.groupby("PIN")
         all_patient_surgery_dicts = []
         if rp.surgery_code_start is None:
-            patient_surgery_claims = data["ITEM"].value_counts()
-            raise ValueError("This won't work")
+            filename = self.logger.output_path / f"{rp.surgery_description}_item_counts.csv"
+            patient_surgery_claims = patient_claims["ITEM"].value_counts().to_csv(filename)
         else:
             patient_surgery_claims = data.loc[(data["ITEM"] >= rp.surgery_code_start) & (data["ITEM"] < rp.surgery_code_end), "ITEM"].value_counts()
 
-        all_hip_claims = patient_surgery_claims.index
-        no_hip_claim_patients = []
-        patient_surgery_dict = {}
-        for name, group in tqdm(patient_groups):
-            claimed_items = "None"
-            un = group["ITEM"].unique().tolist()
-            for item in all_hip_claims:
-                if item in un:
-                    claimed_items = f"{claimed_items}+{item}"
-            
-            count = patient_surgery_dict.get(claimed_items, 0) + 1
-            patient_surgery_dict[claimed_items] = count
-            if claimed_items == 'None':
-                no_hip_claim_patients.append(name)
+            all_hip_claims = patient_surgery_claims.index
+            no_hip_claim_patients = []
+            patient_surgery_dict = {}
+            for name, group in tqdm(patient_groups):
+                claimed_items = "None"
+                un = group["ITEM"].unique().tolist()
+                for item in all_hip_claims:
+                    if item in un:
+                        claimed_items = f"{claimed_items}+{item}"
+                
+                count = patient_surgery_dict.get(claimed_items, 0) + 1
+                patient_surgery_dict[claimed_items] = count
+                if claimed_items == 'None':
+                    no_hip_claim_patients.append(name)
 
-        all_patient_surgery_dicts.append(patient_surgery_dict)
+            all_patient_surgery_dicts.append(patient_surgery_dict)
 
-        # hip_dicts = self.hip_dicts
-        # regions = "Nation,ACT+NSW,VIC+TAS,NT+SA,QLD,WA"
-        # header = f"Item,{regions}\n"
-        regions = "Nation"
-        header = f"Item,{regions}\n"
-        filename = self.logger.output_path / f"{rp.surgery_description}_surgery_item_counts.csv"
-        with open(filename, 'w+') as f:
-            f.write(header)
-            for item in sorted(all_patient_surgery_dicts[0].keys()):
-                line = item.rsplit('+')
-                if len(line) > 1:
-                    line = [x for x in line if x != "None"]
-                    line.sort()
+            # hip_dicts = self.hip_dicts
+            # regions = "Nation,ACT+NSW,VIC+TAS,NT+SA,QLD,WA"
+            # header = f"Item,{regions}\n"
+            regions = "Nation"
+            header = f"Item,{regions}\n"
+            filename = self.logger.output_path / f"{rp.surgery_description}_surgery_item_counts.csv"
+            with open(filename, 'w+') as f:
+                f.write(header)
+                for item in sorted(all_patient_surgery_dicts[0].keys()):
+                    line = item.rsplit('+')
+                    if len(line) > 1:
+                        line = [x for x in line if x != "None"]
+                        line.sort()
 
-                line = ' and '.join(line)
+                    line = ' and '.join(line)
 
-                for i in range(len(all_patient_surgery_dicts)):
-                    count = all_patient_surgery_dicts[i].get(item, 0)
-                    line = f"{line},{count}"
+                    for i in range(len(all_patient_surgery_dicts)):
+                        count = all_patient_surgery_dicts[i].get(item, 0)
+                        line = f"{line},{count}"
 
-                line = f"{line}\n"
-                f.write(line)
+                    line = f"{line}\n"
+                    f.write(line)
 
-        # get alternate surgery items per patient in the data
-        if rp.alternate_code_check_start is not None:
-            alternate_surgery_claims = 0
-            for name in tqdm(no_hip_claim_patients):
-                g = patient_groups.get_group(name)
-                items = g.loc[(g["ITEM"] >= rp.alternate_code_check_start) & (g["ITEM"] < rp.alternate_code_check_end), "ITEM"].unique()
-                if len(items) > 0:
-                    alternate_surgery_claims += 1
+            # get alternate surgery items per patient in the data
+            if rp.alternate_code_check_start is not None:
+                alternate_surgery_claims = 0
+                for name in tqdm(no_hip_claim_patients):
+                    g = patient_groups.get_group(name)
+                    items = g.loc[(g["ITEM"] >= rp.alternate_code_check_start) & (g["ITEM"] < rp.alternate_code_check_end), "ITEM"].unique()
+                    if len(items) > 0:
+                        alternate_surgery_claims += 1
 
-            self.log(f"{alternate_surgery_claims} of {len(no_hip_claim_patients)} had an alternate surgery")
+                self.log(f"{alternate_surgery_claims} of {len(no_hip_claim_patients)} had an alternate surgery")
         
