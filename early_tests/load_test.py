1a6e871898f01101d91faafb3db023c68df70258
Elenchus <mailjpk@gmail.com>
2020-06-01 13:45:17 +1000

    Removal and renaming

--- early_tests/load_test.py
+++
@@ -1,8 +0,0 @@
-from phd_utils import file_utils, graph_utils
-import pandas
-import timeit as tm
-
-filename = file_utils.get_mbs_files()[0]
-timer = tm.Timer("pandas.read_parquet(filename, columns=['PIN', 'ITEM', 'INHOSPITAL'])", globals=globals())
-x = timer.timeit(1)
-print(x)
