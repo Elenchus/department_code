cda542e6cd0ea00380ac0675146cfab75b485a94
Elenchus <mailjpk@gmail.com>
2020-06-01 13:51:44 +1000

    Further removals and renames

--- quick_load_mbs.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from phd_utils import file_utils
-
-mbs_filename = file_utils.get_mbs_files(["2014"])[0]
-data = pd.read_parquet(mbs_filename, columns=["PIN", "ITEM", "MDV_NUMSERV", "DOS"])
